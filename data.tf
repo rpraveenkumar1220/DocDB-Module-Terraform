71317467a8271026e52cbabd77c54cfaa5068be3
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-01-13 16:45:32 +0530

    Docdb adjustments

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ssm_parameter" "username" {
-  name = "roboshop.${var.env}.mongodb.username"
+  name = "roboshop.${var.env}.docdb.username"
 }
 
 data "aws_ssm_parameter" "password" {
-  name = "roboshop.${var.env}.mongodb.password"
+  name = "roboshop.${var.env}.docdb.password"
 }
